d05ce76387c079e8558182e50a3baf9804d5cafa
mason roberts <mrobe18@gmail.com>
2020-11-20 19:46:14 -0500

    user registered, added to db, jwt returned

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -54,14 +56,26 @@ router.post('/',
 
       await user.save()
       // Return json webtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
 
-      res.send('User Registered')
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token })
+        }
+      )
 
     } catch (err) {
       console.error(err.message)
       res.send(500).send('Server Error')
     }
-
   })
 
 module.exports = router
