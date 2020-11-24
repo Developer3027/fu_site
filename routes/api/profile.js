99d24963f9374b352a8638708e80c2cae72ae855
mason roberts <mrobe18@gmail.com>
2020-11-24 16:56:35 -0500

    can add and delete exprience and education from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -79,7 +79,6 @@ router.post('/', [auth,
 
     await profile.save()
     return res.json(profile)
-
   } catch (error) {
     console.error({ msg: error.message })
     res.status(500).json('Server barfed. Oops')
@@ -96,7 +95,7 @@ router.get('/', async (req, res) => {
     res.json(profiles)
   } catch (err) {
     console.error(err.message)
-    res.status(500).json('Ohh, so that happened..')
+    res.status(500).json('Ohh, so that happened.. Server barfed')
   }
 })
 
@@ -115,7 +114,7 @@ router.get('/user/:user_id', async (req, res) => {
       return res.status(400).json({ msg: 'Nope, No profile for that id' })
     }
     console.error(err.message)
-    res.status(500).json('Ohh, so that happened..')
+    res.status(500).json({ msg: 'Ohh, so that happened..' })
   }
 })
 
@@ -135,6 +134,143 @@ router.delete('/', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message)
     res.status(500).json('Ohh, so that happened..')
+  }
+})
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put('/experience', [auth, [
+  check('title', 'I can not let you go without a title').not().isEmpty(),
+  check('company', 'Seriously, take a look at the company').not().isEmpty(),
+  check('from', 'You worked from and to. Gotta have it.')
+]], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  } = req.body
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    // unshift puts it at the start
+    profile.experience.unshift(newExp)
+
+    await profile.save()
+
+    return res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server say whaaaa?')
+  }
+})
+
+// @route   DELETE api/profile/experience/;exp_id
+// @desc    Delete a profile experience
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    // Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+
+    profile.experience.splice(removeIndex, 1)
+
+    await profile.save()
+
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server say whaaaa?')
+  }
+})
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put('/education', [auth, [
+  check('school', 'Alright professor, you need a school').not().isEmpty(),
+  check('degree', 'Seriously, you need a degree').not().isEmpty(),
+  check('fieldofstudy', 'Seriously, what did you study?').not().isEmpty(),
+  check('from', 'How long were you in school? Gotta have it.')
+]], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  } = req.body
+
+  const newEdu = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  }
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    // unshift puts it at the start
+    profile.education.unshift(newEdu)
+
+    await profile.save()
+
+    return res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server say whaaaa?')
+  }
+})
+
+// @route   DELETE api/profile/education/;edu_id
+// @desc    Delete a profile education
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    // Get remove index
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id)
+
+    profile.education.splice(removeIndex, 1)
+
+    await profile.save()
+
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server say whaaaa?')
   }
 })
 
